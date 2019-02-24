ec0286d0b42e3f03c7593fc764498b8591caf55d
Kayamori Takeru <TAKERU@Kayamori-no-MacBook-Pro.local>
2019-02-24 15:08:29 -0800

    init

--- src/App.js
+++ src/App.js
@@ -6,20 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hello World</h1>
+        <div className="square">logo</div>
+        <div>
+          <button>Create Account</button>
+          <button>Login</button>
+        </div>
       </div>
     );
   }
